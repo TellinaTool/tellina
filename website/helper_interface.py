cdac0103deaf27cffd74cf9ffb84281110be2078
Victoria Lin <xilin@cs.washington.edu>
2017-05-10 14:13:31 -0700

    run Tellina demo on CPU

--- website/helper_interface.py
+++ website/helper_interface.py
@@ -15,7 +15,7 @@ from encoder_decoder import data_utils
 from encoder_decoder import decode_tools
 from encoder_decoder import translate as trans
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -26,7 +26,7 @@ FLAGS.num_nn_slot_filling = 5
 FLAGS.normalized = True
 FLAGS.encoder_topology = 'birnn'
 
-FLAGS.sc_token_dim = 200
+FLAGS.sc_token_dim = 150
 FLAGS.batch_size = 32
 FLAGS.num_layers = 1
 FLAGS.learning_rate = 0.0001
@@ -39,14 +39,16 @@ FLAGS.tg_token_use_attention = True
 FLAGS.tg_token_attn_fun = 'non-linear'
 FLAGS.attention_input_keep = 1.0
 FLAGS.attention_output_keep = 1.0
-FLAGS.beta = 0.8
+FLAGS.beta = 0.0
 
 FLAGS.decoding_algorithm = 'beam_search'
 FLAGS.beam_size = 100
 FLAGS.alpha = 1.0
 
-FLAGS.sc_vocab_size = 3076
-FLAGS.tg_vocab_size = 3113
+FLAGS.sc_token_embedding_size = 2824
+FLAGS.sc_vocab_size = 2824
+FLAGS.tg_token_embedding_size = 3112
+FLAGS.tg_vocab_size = 3112
 
 FLAGS.dataset = 'bash'
 FLAGS.data_dir = os.path.join(learning_module_dir, "data", FLAGS.dataset)
@@ -62,17 +64,16 @@ vocabs = data_utils.load_vocab(FLAGS)
 
 if FLAGS.fill_argument_slots:
     # create slot filling classifier
-    model_param_dir = os.path.join(FLAGS.model_dir, 'train.{}.mappings.X.Y.npz'
-                           .format(FLAGS.sc_vocab_size))
+    model_param_dir = os.path.join(FLAGS.model_dir, 'train.mappings.X.Y.npz')
     train_X, train_Y = data_utils.load_slot_filling_data(model_param_dir)
-    slot_filling_classifier = \
-            classifiers.KNearestNeighborModel(FLAGS.num_nn_slot_filling,
-					      train_X, train_Y)
+    slot_filling_classifier = classifiers.KNearestNeighborModel(
+        FLAGS.num_nn_slot_filling, train_X, train_Y)
     print('Slot filling classifier parameters loaded.')
 else:
     slot_filling_classifier = None
 
 def translate_fun(sentence, slot_filling_classifier=slot_filling_classifier):
     print('start running translation model')
+    print(slot_filling_classifier)
     return decode_tools.translate_fun(sentence, sess, model, vocabs, FLAGS,
                                       slot_filling_classifier)
