277ff0d5e07d772fd7af225521177459dc9e965e
Victoria Lin <xilin@cs.washington.edu>
2017-06-21 18:44:27 -0700

    add more objects to administration panel

--- website/admin.py
+++ website/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Annotation, NLRequest, Translation, User
+from .models import Annotation, NLRequest, Translation, URL, URLTag, User
 
+admin.site.register(URL)
 admin.site.register(User)
 admin.site.register(NLRequest)
 admin.site.register(Translation)
 admin.site.register(Annotation)
+admin.site.register(URLTag)
