69886523b08dea6c5a042fe5ec20dcd49a7f7c4c
Victoria Lin <xilin@cs.washington.edu>
2017-08-29 14:10:23 -0700

    add constants file

---
+++ website/constants.py
@@ -0,0 +1,39 @@
+INVALID_ANNOTATION_TAGS = {
+    'NA',
+    'ERROR',
+    'DUPLICATE',
+    'WRONG',
+    'I DON\'T KNOW'
+}
+
+WHITE_LIST = {
+    'find',
+    'xargs'
+}
+
+BLACK_LIST = {
+    'cpp',
+    'g++',
+    'java',
+    'perl',
+    'python',
+    'ruby',
+    'nano',
+    'emacs',
+    'vim'
+}
+
+GREY_LIST = {
+    'alias',
+    'unalias',
+    'set',
+    'unset',
+    'screen',
+    'apt-get',
+    'brew',
+    'yum',
+    'export',
+    'shift',
+    'exit',
+    'logout'
+}
