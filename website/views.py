763ee4bf1706956db0c7277b8e84ac98b6ed1836
Victoria Lin <xilin@cs.washington.edu>
2017-10-17 01:07:39 -0700

    minor fixes

--- website/views.py
+++ website/views.py
@@ -14,7 +14,7 @@ sys.path.append(os.path.join(
 
 from bashlint import data_tools
 
-WEBSITE_DEVELOP = True
+WEBSITE_DEVELOP = False
 CACHE_TRANSLATIONS = False
 
 from website.models import NL, Command, NLRequest, URL, Translation, Vote, User
